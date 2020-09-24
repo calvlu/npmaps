e055698ecefc971bf413c984f094acd2b631f501
Calvin Lu <calvlu@users.noreply.github.com>
2020-09-24 12:08:01 -0400

    popup on click
    added popup on click, set maxbounds, and disabled scroll zoom

--- m1.js
+++ m1.js
@@ -1,49 +1,113 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiaW1hZGF2ZWxvcGVyIiwiYSI6ImNrZTdxZ3RiNjBydnYycXRmZm5vbzJnMm0ifQ.HvEjXY7eSD4PaQTL1Uhv6Q';
-    var map = new mapboxgl.Map({
-        container: 'map', // container id
-        style: 'mapbox://styles/imadaveloper/ckew482j30e5p19l08q0bytuj', // style URL
-        center: [-90.194329, 15.819034], // starting position [lng, lat]
-        zoom: 7.2 // starting zoom
-    });
 
-
+var bounds = [
+  [-95.3566105298246, 13.0517966258875], // Southwest coordinates, , 
+  [-85.2111927824501, 18.416632999923] // Northeast coordinates, 
+];
+var map = new mapboxgl.Map({
+  container: 'map', // container id
+  style: 'mapbox://styles/mapbox/light-v10', // style URL
+  center: [-90.3900017126126, 15.6055893260292], // starting position [lng, lat]
+  zoom: 3,
+  maxBounds: bounds,
+  scrollZoom: false
+});
+map.addControl(new mapboxgl.NavigationControl({
+  showCompass: false
+}));
 map.on('load', function() {
 
+  map.addSource('nonprofits', {
+    type: 'vector',
+    url: 'mapbox://imadaveloper.b29hf9g8'
+  });
 
+  map.addLayer({
+    'id': 'npos',
+    'type': 'circle',
+    'source': 'nonprofits',
+    'source-layer': 'nonprofitonetoone-clf0cp',
+    'paint': {
+      'circle-radius': 5,
+      'circle-opacity': 0.6,
+      // color circles by partner status, using a match expression
+      // https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-match
+      'circle-color': [
+        'match', ['get', 'PioneroAffiliation'],
+        'P',
+        '#6032ec',
+        /* other */
+        '#cfcdd5'
+      ]
+    }
+  });
+  map.on('click', 'npos', function(e) {
+    popup.remove();
+
+    var coordinates = e.features[0].geometry.coordinates.slice();
+    var description = '<h3>' + e.features[0].properties.NPO + '</h3>' + 
+      '<p>Pionero Affiliation: ' + e.features[0].properties.PioneroAffiliation + '</p>' +
+      '<p>Year Founded: ' + e.features[0].properties.YearFounded + '</p>' +
+      '<p>Nonprofit Size: ' + e.features[0].properties.Size + '</p>' +
+      '<p>Annual Budget: ' + e.features[0].properties.Budget + '</p>' +
+      '<p>Tax Registration: ' + e.features[0].properties.USTaxStatus + '</p>';
+    // Ensure that if the map is zoomed out such that multiple
+    // copies of the feature are visible, the popup appears
+    // over the copy being pointed to.
+    while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+      coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+    }
+
+    new mapboxgl.Popup()
+      .setLngLat(coordinates)
+      .setHTML(description)
+      .addTo(map);
+  });
   var popup = new mapboxgl.Popup({
     closeButton: false,
     closeOnClick: false
   });
- 
-  map.on('mouseenter', 'nppartners', function (e) {
-// Change the cursor style as a UI indicator.
+
+  map.on('mouseenter', 'npos', function(e) {
+    // Change the cursor style as a UI indicator.
     map.getCanvas().style.cursor = 'pointer';
-/*
-  var partners = map.queryRenderedFeatures(e.point, {
-    layers: ['nppartners']
-  });
- */
+    /*
+      var partners = map.queryRenderedFeatures(e.point, {
+        layers: ['nppartners']
+      });
+     */
     //var coordinates = [e.features[0].properties.Longitude, e.features[0].properties.Latitude];
     var coordinates = e.features[0].geometry.coordinates.slice();
 
-    var npname = e.features[0].properties.Nonprofit;
+    var npname = e.features[0].properties.NPO;
 
- 
-// Ensure that if the map is zoomed out such that multiple
-// copies of the feature are visible, the popup appears
-// over the copy being pointed to.
+
+    // Ensure that if the map is zoomed out such that multiple
+    // copies of the feature are visible, the popup appears
+    // over the copy being pointed to.
 
     while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
       coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
     }
- 
-// Populate the popup and set its coordinates
-// based on the feature found.
-    popup.setLngLat(coordinates).setHTML('<h3>' + npname + '</h3><p>'+ 'insert descrip' +'</p>').addTo(map);
+
+    // Populate the popup and set its coordinates
+    // based on the feature found.
+    popup.setLngLat(coordinates).setHTML(npname).addTo(map);
   });
- 
-  map.on('mouseleave', 'nppartners', function () {
+
+
+  map.on('mouseleave', 'npos', function() {
     map.getCanvas().style.cursor = '';
     popup.remove();
   });
+
+  function flyToStore(currentFeature) {
+    map.flyTo({
+      center: currentFeature.geometry.coordinates,
+      zoom: 15
+    });
+  }
+
+
+
 });
